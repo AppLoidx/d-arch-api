873cb82cc4f6c6d26d5f5df0e45daccb291d1cb2
Lilith <apploidyakutsk@gmail.com>
2021-10-07 12:08:26 +0300

    first commit

---
+++ src/test/java/com/apploidxxx/darchapi/DArchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apploidxxx.darchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DArchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
